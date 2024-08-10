ae5901fffae42ec4a710e1a4ba72901912f09faa
Elliott Hartmann Ulriksen <140018175+noibs@users.noreply.github.com>
2024-08-10 23:19:23 +0200

    Dark mode + nice switch

--- components/Buttons/ThemeBtn/index.tsx
+++ components/Buttons/ThemeBtn/index.tsx
@@ -1,33 +1,45 @@
 // This component contains the theme button, which when pressed changes between light and dark mode. (Not yet implemented)
 'use client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { cubicBezier, motion, useAnimationControls } from 'framer-motion';
 
 // This varaible is used to prevent the user from clicking the button multiple times
 let isFetching = false;
 
-const ThemeBtn = ({
-  styles,
-  click,
-}: {
-  styles: string;
-  click?: () => void;
-}) => {
+const ThemeBtn = ({ styles }: { styles: string }) => {
+  const [isLightTheme, setIsLightTheme] = useState(true);
   const controls = useAnimationControls();
+
+  // This function changes the theme of the page
+  const changeTheme = () => {
+    if (isLightTheme) {
+      document.body.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.body.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+    }
+  };
 
   // This function is called when the user clicks the button, the click function is provided by the parent component
   const handleClick = () => {
     if (isFetching) return;
-    if (click) click();
+    changeTheme();
     isFetching = true;
 
     // Animation
     const easing = cubicBezier(0.25, 1, 0.5, 1);
-
     controls.start({
-      rotate: 900,
+      rotate: 720,
       transition: { duration: 1, ease: easing },
     });
+
+    setTimeout(
+      () => {
+        setIsLightTheme(!isLightTheme);
+      },
+      isLightTheme ? 100 : 200
+    );
 
     setTimeout(() => {
       isFetching = false;
@@ -35,16 +47,24 @@ const ThemeBtn = ({
     }, 2000);
   };
 
+  useEffect(() => {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+      setIsLightTheme(theme === 'light');
+      document.body.setAttribute('data-theme', theme);
+    } else {
+      localStorage.setItem('theme', 'light');
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }, []);
+
   return (
-    <>
-      <button
-        className={styles}
-        onClick={handleClick}
-        aria-label="Change Theme"
-      >
-        <i className="fa-solid fa-moon"></i>
-      </button>
-    </>
+    <button className={styles} onClick={handleClick} aria-label="Change Theme">
+      <motion.i
+        className={`fa-solid ${isLightTheme ? 'fa-moon' : 'fa-sun'}`}
+        animate={controls}
+      ></motion.i>
+    </button>
   );
 };
 
--- components/Watermark/index.tsx
+++ components/Watermark/index.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.scss';
 
 const Watermark = () => {
   return (
-    <div className={styles.logoContainer}>
+    <div id="logo" className={styles.logoContainer}>
       <a target="_blank" href="https://ellioott.me" className={styles.logo}>
         <h1>
           Elliott<span className={styles.dot}>.</span>
